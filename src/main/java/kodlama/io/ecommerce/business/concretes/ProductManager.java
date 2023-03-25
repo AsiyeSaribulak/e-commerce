ea7830329dbc45a46b3e720756ff7730f05f4025
Asiye Sarıbulak <5364542@ogrenci.bilecik.edu.tr>
2023-03-25 17:25:07 +0300

    ecommerce

---
+++ src/main/java/kodlama/io/ecommerce/business/concretes/ProductManager.java
@@ -0,0 +1,71 @@
+package kodlama.io.ecommerce.business.concretes;
+
+import kodlama.io.ecommerce.business.abstracts.ProductService;
+import kodlama.io.ecommerce.entities.concretes.Product;
+import kodlama.io.ecommerce.repository.abstracts.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductManager implements ProductService {
+    private final ProductRepository repository;
+
+    public ProductManager(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Product getById(int id) {
+        checkIfBrandExists(id);
+        return repository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public Product add(Product product) {
+        validateProduct(product);
+        return repository.save(product);
+    }
+
+    @Override
+    public Product update(int id, Product product) {
+        product.setId(id);
+        validateProduct(product);
+        return repository.save(product);
+    }
+
+    @Override
+    public void delete(int id) {
+        checkIfBrandExists(id);
+        repository.deleteById(id);
+    }
+
+
+    private void validateProduct(Product product) {
+        checkIfUnitPriceValid(product);
+        checkIfQuantityValid(product);
+        checkIfDescriptionLengthValid(product);
+    }
+
+    private void checkIfUnitPriceValid(Product product) {
+        if (product.getUnitPrice() <= 0)
+            throw new IllegalArgumentException("Price cannot be less than or equal to zero.");
+    }
+
+    private void checkIfQuantityValid(Product product) {
+        if (product.getQuantity() < 0) throw new IllegalArgumentException("Quantity cannot be less than zero.");
+    }
+
+    private void checkIfDescriptionLengthValid(Product product) {
+        if (product.getDescription().length() < 10 || product.getDescription().length() > 50)
+            throw new IllegalArgumentException("Description length must be between 10 and 50 characters.");
+    }
+    private void checkIfBrandExists(int id){
+        if(!repository.existsById(id)) throw new RuntimeException("There is no product!");
+    }
+}
---
+++ src/main/java/kodlama/io/ecommerce/entities/concretes/Product.java
@@ -0,0 +1,25 @@
+package kodlama.io.ecommerce.entities.concretes;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private int quantity;
+    private double unitPrice;
+    private String description;
+
+
+}
